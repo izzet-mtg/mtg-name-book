9c96d606d8bed7b48033938b7bb8226700702098
moba1 <moba2@protonmail.com>
2025-08-16 00:54:21 +0900

    Scryfall のページへ飛べるようにした

--- popup.tsx
+++ popup.tsx
@@ -47,7 +47,9 @@ function IndexPopup() {
                   </p>
                 )}
               </div>
-              <a href={card.scryfall_uri} className="px-2 py-2 rounded-lg w-15 text-sm font-medium text-center text-slate-100 dark:text-slate-200 bg-slate-600 hover:bg-slate-700 focus:ring-2 focus:outline-none focus:ring-slate-400 dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-400">Scryfall</a>
+              <div className="flex justify-center items-center">
+                <a href={card.scryfall_uri} target="_blank" rel="noopener noreferrer" className="px-2 py-2 rounded-lg w-15 h-10 text-sm text-align font-medium text-slate-100 dark:text-slate-200 bg-slate-600 hover:bg-slate-700 focus:ring-2 focus:outline-none focus:ring-slate-400 dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-400">Scryfall</a>
+              </div>
             </div>
           </li>
         ))}
