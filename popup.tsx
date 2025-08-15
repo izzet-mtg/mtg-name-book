b18c680dab6df23d955639a0cc3674468d3f1e01
moba1 <moba2@protonmail.com>
2025-08-15 19:41:09 +0900

    入力欄と入力内容を変換するボタンを設置

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,20 @@
-import { useState } from "react"
+import React, { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [cardNameCandidate, setCardNameCandidate] = useState("")
+  const [cardName, setCardName] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <input
+        id="origin-card-name"
+        type="text"
+        onInput={(event: React.FormEvent<HTMLInputElement>) =>
+          setCardNameCandidate(event.currentTarget.value)
+        }
+      />
+      <button onClick={() => setCardName(cardNameCandidate)}>変換</button>
+      <p>{cardName}</p>
     </div>
   )
 }
