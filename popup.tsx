2196dfc4f2690980939ac56e412584a149b53c1f
moba1 <moba2@protonmail.com>
2025-08-16 00:41:02 +0900

    カードのリスト表示を調整

--- popup.tsx
+++ popup.tsx
@@ -33,17 +33,22 @@ function IndexPopup() {
         onClick={() => setCardName(cardNameCandidate)}>
         変換
       </button>
-      <ul className="w-full divide-slate-500">
+      <ul className="w-full divide-slate-500 divide-y">
         {cards.map((card, index) => (
-          <li key={`card-${index}`} className="p-1">
-            <p className="text-sm font-medium text-slate-800 dark:text-slate-300 truncate">
-              {card.name}
-            </p>
-            {card.printed_name && (
-              <p className="text-sm text-slate-500 dark:text-slate-400 truncate pl-2">
-                — {card.printed_name}
-              </p>
-            )}
+          <li key={`card-${index}`} className="p-1 py-2">
+            <div className="grid grid-cols-[1fr_auto] gap-2">
+              <div>
+                <p className="text-sm font-medium text-slate-800 dark:text-slate-300 truncate">
+                  {card.name}
+                </p>
+                {card.printed_name && (
+                  <p className="text-sm text-slate-500 dark:text-slate-400 truncate pl-2">
+                    — {card.printed_name}
+                  </p>
+                )}
+              </div>
+              <a href={card.scryfall_uri} className="px-2 py-2 rounded-lg w-15 text-sm font-medium text-center text-slate-100 dark:text-slate-200 bg-slate-600 hover:bg-slate-700 focus:ring-2 focus:outline-none focus:ring-slate-400 dark:bg-slate-600 dark:hover:bg-slate-500 dark:focus:ring-slate-400">Scryfall</a>
+            </div>
           </li>
         ))}
       </ul>
